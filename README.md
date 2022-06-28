9a2836fe556e8f241e71a38a81c6e695c37b76d2
Richard Jackson <rpjackson2@gmail.com>
2022-06-28 15:00:28 -0500

    Removed relative file path from images FIXED*

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# videoworks-frontend
-# videoworks-frontend
+
 # videoworks-front
--- js/heroImages.js
+++ js/heroImages.js
@@ -2,36 +2,39 @@
 // TODO: NOTES:
 // using imperative programming to create new hero images and randomly show a new image on reload
 
-
-
 let heroGraphicDiv = document.querySelector(".hero-graphic");
 
 // pointing man
 const pointingPerson = new Image(450);
-pointingPerson.src = "../images/pointingPerson33.png";
+pointingPerson.src = "images/pointingPerson33.png";
 pointingPerson.classList.add("pointing-man-animation");
 
 // floating woman
 const floatingWoman = new Image(525);
-floatingWoman.src = "../images/MetaverseGirl(1).png";
+floatingWoman.src = "images/MetaverseGirl(1).png";
 floatingWoman.classList.add("floating-woman-animation");
 
 // leaning man
 const leaningMan = new Image(450);
-leaningMan.src = "../images/3d-Metaverse-man(2).png";
+leaningMan.src = "images/3d-Metaverse-man(2).png";
 leaningMan.classList.add("leaning-man-animation");
 
 // diving woman
 const divingWoman = new Image(505);
-divingWoman.src = "../images/Metaverse Girl (4).png";
+divingWoman.src = "images/Metaverse Girl (4).png";
 divingWoman.classList.add("diving-woman-animation");
 
 // array of image objects
-let floatingHeroImages = [pointingPerson, floatingWoman, leaningMan, divingWoman];
+let floatingHeroImages = [
+	pointingPerson,
+	floatingWoman,
+	leaningMan,
+	divingWoman,
+];
 
 // random image generator (from floatingHeroImages array)
 let setFloatingHeroImage = () => {
-    heroGraphicDiv.appendChild(floatingHeroImages[Math.floor(Math.random() * 4)])
+	heroGraphicDiv.appendChild(floatingHeroImages[Math.floor(Math.random() * 4)]);
 };
 
 // function call
